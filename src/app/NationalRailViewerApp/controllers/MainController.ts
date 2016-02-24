66506c78b9d447cdb9a9ae584bd21959b5e18ea7
Gareth Oates <goatie@gmail.com>
2016-02-24 15:58:47 +0100

    Added watches to Scripts, Html and CSS.  Removed toastr and dependencies.  Added error property to output which shows on error using bootstrap alert div.

--- src/app/NationalRailViewerApp/controllers/MainController.ts
+++ src/app/NationalRailViewerApp/controllers/MainController.ts
@@ -1,13 +1,15 @@
 /// <reference path="../../../../typings/tsd.d.ts" />
 namespace app.controllers {
+
     export class MainController {
 
         public city: string;
         public departures: Interfaces.IQueryResult;
         public arrivals: Interfaces.IQueryResult;
+        public error: string;
 
-        static $inject: Array<string> = ['nationalRail', '$interval', '$routeParams', 'toastr'];
-        constructor(private NationalRail: app.Interfaces.INationalRailService, private $interval: ng.IIntervalService, private $routeParams: Interfaces.IParameters, private toastr: Toastr) {
+        static $inject: Array<string> = ['nationalRail', '$interval', '$routeParams'];
+        constructor(private NationalRail: app.Interfaces.INationalRailService, private $interval: ng.IIntervalService, private $routeParams: Interfaces.IParameters) {
             this.city = $routeParams.City;
             this.GetData();
             this.$interval(() => {
@@ -15,26 +17,18 @@ namespace app.controllers {
             }, 60000);
         }
 
-        isErrorRaised: boolean = false;
-        onError($error: string) {
-            if (!this.isErrorRaised) {
-                toastr.error("Could not load data for '" + this.city + "'!", 'Error');
-                this.isErrorRaised = !this.isErrorRaised;
-            }
-        };
-
         GetData(): void {
             this.NationalRail.getDepartures(this.city).then((data: app.Interfaces.IQueryResult) => {
                 this.departures = data;
-            }, this.onError);
+            }, () => { this.error = "No data found for '" + this.city + "'"; });
 
             this.NationalRail.getArrivals(this.city).then((data: app.Interfaces.IQueryResult) => {
                 this.arrivals = data;
-            }, this.onError);
+            }, () => { this.error = "Unable to load data for '" + this.city + "'"; });
         }
     }
 
     var appModule = angular.module('nationalRailViewer')
-    appModule.controller('MainController', ['nationalRail', '$interval', '$routeParams', 'toastr',
-        (nationalRail: app.Interfaces.INationalRailService, $interval: ng.IIntervalService, $routeParams: Interfaces.IParameters, toastr: Toastr) => new MainController(nationalRail, $interval, $routeParams, toastr)]);
+    appModule.controller('MainController', ['nationalRail', '$interval', '$routeParams',
+        (nationalRail: app.Interfaces.INationalRailService, $interval: ng.IIntervalService, $routeParams: Interfaces.IParameters) => new MainController(nationalRail, $interval, $routeParams)]);
 }
