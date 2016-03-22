aa370f2ec589b9cc1e9aaecc30a31fe89046aeaf
Gareth Oates <goatie@gmail.com>
2016-03-22 13:29:21 +0100

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,6 @@
 
 if [[ $TRAVIS_BRANCH == 'master' ]] ; then
     echo $PWD
-    cd public
     git init
 
     git config user.name "Travis CI"
@@ -13,7 +12,7 @@ if [[ $TRAVIS_BRANCH == 'master' ]] ; then
 
     # We redirect any output to
     # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-    git push --force --quiet "https://${git_user}:${git_password}@${git_target}" master:master > /dev/null 2>&1
+    git push --force --quiet "https://GIT_USER:GIT_PASSWORD@$GIT_TARGET" master:master > /dev/null 2>&1
 else
     echo 'Invalid Branch.  You can only deploy from master.'
     exit 1
