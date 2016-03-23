05fda7faa5ba1eae78776bea31ccca3fa57da096
Gareth Oates <goatie@gmail.com>
2016-03-23 22:46:56 +0100

    Updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,8 @@ if [[ $TRAVIS_BRANCH == 'master' ]] ; then
 
     # We redirect any output to
     # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-    git push --force --quiet "https://goatie:GIT_PASSWORD@$GIT_TARGET" master:master # > dev/null
+    git push --force --quiet "https://goatie@$GIT_TARGET" master:master # > dev/null
+    GIT_PASSWORD
 else
     echo 'Invalid Branch.  You can only deploy from master.'
     exit 1
